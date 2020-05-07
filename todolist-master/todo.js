7e27f0b4a9d98733d4ae12539927deccfb101788
Dakiri Mina <57801216+MinaDakiri12@users.noreply.github.com>
2020-05-07 23:41:55 +0000

    change in js

--- todolist-master/todo.js
+++ todolist-master/todo.js
@@ -1,38 +1,47 @@
-//ADD new To do 
-const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos');
-const popup = document.querySelector('.popup');
-const gPop = document.querySelector('.popup-wrapper');
-const btn = document.querySelector('.btn');
-const search = document.querySelector('.search input');
+
+
+/ADD new To do
+const search = document.querySelector('.search input');//18
+
+const list = document.querySelector('.todos');  //23
+
+const addForm = document.querySelector('.add'); //43
+const btn = document.querySelector('.btn');     //47
+
+const gPop = document.querySelector('.popup-wrapper');//58
+const popup = document.querySelector('.popup'); //59
+
 gPop.style.display = "none";
 
 
 /***************reusable function********************/
 
 /* Function pour l'alert et le popup qui va etre afficher (time control)*/
-function start(duree) {
-   var o = document.getElementById("sp");
-   if (duree > 0) {
-      o.innerHTML = duree;
-      gPop.style.display = "block";
-      setTimeout("start(" + duree + " -1)", 1000);
-   } else {
-      alert("enter a valid to do");
-      o.innerHTML = "Au revoir";
-      gPop.style.display = "none";
-      popup.style.visibility = "hidden";
+function start(duree)
+{
+var o=document.getElementById("sp");
+if(duree > 0)
+{
+o.innerHTML = duree;
+gPop.style.display = "block";
+setTimeout("start("+duree+" -1)", 1000);
+}
+else
+{
+   alert("enter a valid to do");
+o.innerHTML ="Au revoir";
+gPop.style.display="none";
+popup.style.visibility ="hidden";
 
-   }
-};
+}};
 
 
 /* Function Creation dynamique du POPUP */
 
-function create() {
+function create(){
    const div = document.createElement('div');
    div.classList.add('popup-close');
-   div.setAttribute('id', 'closing');
+   div.setAttribute('id','closing');
    const text = document.createTextNode('X');
    div.appendChild(text);
    popup.append(div);
@@ -43,42 +52,47 @@ function create() {
    <h2>Fill the Input</h2>
    <p>Don't forget</p>
    <a href="#">Return</a>`;
-   div2.innerHTML = html;
-   popup.append(div2);
-
+   div2.innerHTML=html;
+   popup.append(div2); 
+   
 }
 
 /* Function generation dynamique des TODOS */
 
-const generateTemp = todo => {
+const generateTemp = todo =>{
    const html = `
    <li class="list-group-item d-flex justify-content-between align-items-center">
              <span>${todo}</span>
              <i class="fas fa-trash delete"></i>
             </li>
-   `;
+   `;  
    list.innerHTML += html;
+   localStorage[''] = list.innerHTML
 };
+
+if(localStorage['']){
+   list.innerHTML = localStorage[''];
+}
 
 
 /* function pour controller l'evenement et pour ne pas etre repeté à chaque clique */
 function onetime(node, type, callback) {
 
-   node.addEventListener(type, function (e) {
+	node.addEventListener(type, function(e) {
+	
+		e.target.removeEventListener(e.type, arguments.callee);
 
-      e.target.removeEventListener(e.type, arguments.callee);
-
-      return callback(e);
-   });
+		return callback(e);
+	});
 }
 
-onetime(gPop, 'click', handler);
+onetime(gPop,'click',handler);
 
-function handler(e) {
-
-   if (e.target.id = 'closing') {
-
-      gPop.style.display = "none";
+    function handler(e){
+         
+      if(e.target.id='closing'){
+   
+         gPop.style.display ="none";
    }
 }
 
@@ -90,20 +104,23 @@ function handler(e) {
 /************* Adding TO DO**************/
 
 //Eventlistner Add TODOS
-btn.addEventListener('click', e => {
+btn.addEventListener('click',e =>{
    e.preventDefault();
-   let nbr=3;
-   if (document.querySelector('.popup-content') == null) {
+   const add = addForm.add.value;
+   const number = 3;
+   if(document.querySelector('.popup-content') == null){
       create();
    }
-   if (addForm.add.value == "") {
-      start(nbr);
-      popup.style.visibility = "visible";
-   } else {
-       addf = generateTemp(addForm.add.value);
-      localStorage.getItem(addForm.add.value);
+   if (add == ''){
+      start(number);
+      popup.style.visibility = 'visible'
+      
+   }
+   else{
+   generateTemp(add);
    }
 
+   
 });
 
 /************* Fin Adding TO DO**************/
@@ -111,19 +128,14 @@ btn.addEventListener('click', e => {
 
 
 /*************Deleting  TO DO**************/
+list.addEventListener('click',e =>{
 
-
-list.addEventListener('click', e => {
-const i=document.querySelector("i");
-const li=document.querySelector("li");
-   if (i.classList.contains('delete')) {
-
-      li.remove();
+   if(e.target.tagName === 'I'){
+      e.target.parentElement.remove();
+      localStorage[''] = list.innerHTML
    }
 
 });
-
-
 
 /************* Fin Deleting  TO DO**************/
 
@@ -139,30 +151,35 @@ const li=document.querySelector("li");
 
 
 
-const retrieve = (term) => {
+const retrieve = (term) =>{
 
    //function pour faire un filtre i
-};
-
-
-//evenement de recherche des mots clés 
-search.addEventListener('keyup', () => {
-
    var filter, li, a, i, txtValue;
    filter = search.value.toUpperCase();
    li = list.getElementsByTagName("li");
+   
    for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("span")[0];
+      console.log(a);
       txtValue = a.textContent || a.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
          li[i].style.display = "";
       } else {
          li[i].style.setProperty('display', 'none', 'important');
-
+         
       }
    }
 
+};  
+
+
+//evenement de recherche des mots clés 
+search.addEventListener('keyup', () =>{
+
+ retrieve();
+  
 
 })
 
 /*************************************Fin SEARCH ITEM********************************************/
+
